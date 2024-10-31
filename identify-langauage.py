9fb12587c048728201b4946b28c2306c901e7357
Dolica Akello-Egwel <d.akelloegwel@lcc.arts.ac.uk>
2024-10-31 18:41:31 +0000

    just trying some stuff...

---
+++ identify-langauage.py
@@ -0,0 +1,32 @@
+import whisper
+import os
+import numpy as np
+
+
+model = whisper.load_model("turbo")
+
+files = ["japanese", "russian", "german"]
+
+for audio_file in files:
+
+    # load audio and pad/trim it to fit 30 seconds
+    file_path = os.path.join(os.getcwd(), audio_file + ".m4a")
+    print(file_path)
+    audio = whisper.read(file_path)
+    audio = whisper.pad_or_trim(audio)
+
+    # make log-Mel spectrogram and move to the same device as the model
+    mel = whisper.log_mel_spectrogram(audio).to(model.device)
+
+    # detect the spoken language
+    _, probs = model.detect_language(mel)
+    print(f"Detected language: {max(probs, key=probs.get)}")
+
+    # decode the audio
+    options = whisper.DecodingOptions()
+    result = whisper.decode(model, mel, options)
+
+    # print the recognized text
+    print(result.text)
+
+    break
