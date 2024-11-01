c390af697ef2c25acbd6bb43ca229ba59ccd931b
Dolica Akello-Egwel <d.akelloegwel@lcc.arts.ac.uk>
2024-11-01 12:42:58 +0000

    some refactoring

--- identify-langauage.py
+++ identify-langauage.py
@@ -5,8 +5,6 @@ import numpy as np
 
 model = whisper.load_model("turbo")
 
-files = ["japanese", "russian", "german"]
-
 
 def record_audio(duration=5, sample_rate=16000):
     audio = sd.rec(
@@ -16,12 +14,8 @@ def record_audio(duration=5, sample_rate=16000):
     return np.squeeze(audio)
 
 
-for audio_file in files:
+def prepare_audio_and_detect_language(audio):
 
-    # load audio and pad/trim it to fit 30 seconds
-    file_path = os.path.join(os.getcwd(), audio_file + ".m4a")
-    print(file_path)
-    audio = whisper.load_audio(file_path)
     audio = whisper.pad_or_trim(audio)
 
     # make log-Mel spectrogram and move to the same device as the model
@@ -29,21 +23,20 @@ for audio_file in files:
 
     # detect the spoken language
     _, probs = model.detect_language(mel)
-    print(f"Detected language: {max(probs, key=probs.get)}")
+    detected_language = max(probs, key=probs.get)
+    print(f"Detected language: {detected_language}")
+    return detected_language
 
-    # decode the audio
-    # options = whisper.DecodingOptions()
-    # result = whisper.decode(model, mel, options)
 
-    # # print the recognized text
-    # print(result.text)
+files = ["japanese", "russian", "german"]
+
+for audio_file in files:
+
+    # load audio and pad/trim it to fit 30 seconds
+    file_path = os.path.join(os.getcwd(), audio_file + ".m4a")
+    audio = whisper.load_audio(file_path)
+    prepare_audio_and_detect_language(audio)
 
 print("Say something...")
 audio = record_audio()
-audio = whisper.pad_or_trim(audio)
-# make log-Mel spectrogram and move to the same device as the model
-mel = whisper.log_mel_spectrogram(audio, n_mels=128).to(model.device)
-
-# detect the spoken language
-_, probs = model.detect_language(mel)
-print(f"Detected language: {max(probs, key=probs.get)}")
+prepare_audio_and_detect_language(audio)
