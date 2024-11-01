360e5046eb88abe0c0f5926c34f5c6139f1efdb3
DolicaAkelloEgwel <d.akelloegwel@lcc.arts.ac.uk>
2024-11-01 12:32:03 +0000

    attempting microphone thing...

--- identify-langauage.py
+++ identify-langauage.py
@@ -1,9 +1,20 @@
 import whisper
 import os
+import sounddevice as sd
+import numpy as np
 
 model = whisper.load_model("turbo")
 
 files = ["japanese", "russian", "german"]
+
+
+def record_audio(duration=5, sample_rate=16000):
+    audio = sd.rec(
+        int(duration * sample_rate), samplerate=sample_rate, channels=1, dtype="float32"
+    )
+    sd.wait()
+    return np.squeeze(audio)
+
 
 for audio_file in files:
 
@@ -26,3 +37,12 @@ for audio_file in files:
 
     # print the recognized text
     print(result.text)
+
+audio = record_audio()
+audio = whispter.pad_or_trim(audio)
+# make log-Mel spectrogram and move to the same device as the model
+mel = whisper.log_mel_spectrogram(audio, n_mels=128).to(model.device)
+
+# detect the spoken language
+_, probs = model.detect_language(mel)
+print(f"Detected language: {max(probs, key=probs.get)}")
